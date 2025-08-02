2222eddc3bcd82a8f96a9a3d6704409fa253e476
mesternandor04@gmail.com <mesternandor04@gmail.com>
2025-08-02 14:58:44 +0200

    project initialization

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-8 text-center">
+      <h1 className="text-4xl font-bold">Tailwind CSS Test</h1>
+      <p className="mt-4">Ha ez kék háttér fehér szöveggel jelenik meg, akkor működik!</p>
+    </div>
+  )
+}
+export default App
